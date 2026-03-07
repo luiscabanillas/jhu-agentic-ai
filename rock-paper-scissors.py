3a6b9a357b9df39b0bbbe29228a0dc72030939d0
Luis Cabanillas <luis.cabanillasl@gmail.com>
2026-03-06 17:05:53 -0800

    Adding rock paper scissors exercise

---
+++ rock-paper-scissors.py
@@ -0,0 +1,30 @@
+import random
+import sys
+
+print('ROCK, PAPER, SCISSORS')
+
+moves: dict = {'rock': "🪨", 'paper': "📜", 'scissors': "✂️"}
+moves_list = list(moves.keys())
+
+while True:
+    print()
+    print('Enter your move: (rock, paper, scissors)')
+    player_move = input().lower().strip()
+    if player_move == 'quit':
+        sys.exit()
+    if player_move not in moves:
+        print('Invalid move. Try again.')
+        continue
+
+    computer_move = random.choice(moves_list)
+
+    print(f'You played {moves[player_move]}, computer played {moves[computer_move]}.')
+
+    result = (moves_list.index(player_move) - moves_list.index(computer_move)) % 3
+
+    if result == 0:
+        print('It\'s a tie!')
+    elif result == 1:
+        print('You win!')
+    else:
+        print('You lose!')
